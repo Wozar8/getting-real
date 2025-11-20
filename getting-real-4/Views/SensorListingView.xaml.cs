ea62a3c2a63183d50f0be386f39835cd15ed78f6
Wozar <2442498-wozar@users.noreply.gitlab.com>
2025-11-20 23:18:31 +0100

    Added SensorListingView

---
+++ getting-real-4/Views/SensorListingView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using getting_real_4.Models;
+using getting_real_4.Models.Repositories;
+
+namespace getting_real_4.Views;
+
+public partial class SensorListingView : UserControl
+{
+    public SensorListingView()
+    {
+        InitializeComponent();
+    }
+}
