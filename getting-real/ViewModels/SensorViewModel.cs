d8d5619f805d3146f1653b1b04dd99494b6c07c1
Wozar <2442498-wozar@users.noreply.gitlab.com>
2025-11-21 01:44:33 +0100

    Remove unused properties and constructor from SensorViewModel

--- getting-real/ViewModels/SensorViewModel.cs
+++ getting-real/ViewModels/SensorViewModel.cs
@@ -21,16 +21,4 @@ public class SensorViewModel
     public string Keys { get; set; }
     public string SensorType { get; set; }
     public string ConnectionType { get; set; }
-
-    public ICommand AddSensorCommand { get; }
-
-    public SensorViewModel(Sensor sensor)
-    {
-        _senor = sensor;
-        this.Id = _senor.Id.ToString();
-        this.Type = _senor.Type;
-        this.Keys = _senor.Keys;
-        this.SensorType = _senor.SensorType;
-        this.ConnectionType = _senor.ConnectionType;
-    }
 }
