7b8c4857a8cb689a3028feacd48d0722d6002938
Wozar <2442498-wozar@users.noreply.gitlab.com>
2025-11-21 02:05:20 +0100

    Add RegisterSensorViewModel and bind properties in RegisterSensorView

---
+++ getting-real/ViewModels/RegisterSensorViewModel.cs
@@ -0,0 +1,33 @@
+namespace getting_real_4.ViewModels;
+
+public class RegisterSensorViewModel : ViewModelBase
+{
+    private string _sensorType;
+    private string _connectionType;
+    private string _keys;
+    private string _type;
+
+    public string SensorType
+    {
+        get => _sensorType;
+        set => OnPropertyChanged(SensorType);
+    }
+
+    public string ConnectionType
+    {
+        get => _connectionType;
+        set => OnPropertyChanged(ConnectionType);
+    }
+
+    public string Keys
+    {
+        get => _keys;
+        set => OnPropertyChanged(Keys);
+    }
+
+    public string Type
+    {
+        get => _type;
+        set => OnPropertyChanged(Type);
+    }
+}
