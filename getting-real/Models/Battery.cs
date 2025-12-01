8ef37b684561892c792aa639830c12e977eb506a
Wozar <2442498-wozar@users.noreply.gitlab.com>
2025-12-01 18:30:38 +0100

    Added battery class

---
+++ getting-real/Models/Battery.cs
@@ -0,0 +1,17 @@
+﻿namespace getting_real_4.Models;
+
+public class Battery
+{
+    public Battery(DateTime registerDate, string batterType, double maxCapacity, string mountType)
+    {
+        RegisterDate = registerDate;
+        BatteryType = batterType;
+        MaxCapacity = maxCapacity;
+        MountType = mountType;
+    }
+
+    public string BatteryType { get; set; }
+    public double MaxCapacity { get; set; }
+    public string MountType { get; set; }
+    public DateTime RegisterDate { get; set; }
+}
