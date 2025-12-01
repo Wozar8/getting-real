5e0383919e5ceeb4a3d0dbfccb491e5159eaa5be
Wozar <2442498-wozar@users.noreply.gitlab.com>
2025-12-01 11:50:40 +0100

    Changes in UI reflect model layer

--- getting-real/ViewModels/SensorViewModel.cs
+++ getting-real/ViewModels/SensorViewModel.cs
@@ -10,16 +10,47 @@ public class SensorViewModel : ViewModelBase
     public SensorViewModel(Sensor sensor)
     {
         _senor = sensor;
-        Id = _senor.Id.ToString();
-        Type = _senor.Type;
-        Keys = _senor.Keys;
-        SensorType = _senor.SensorType;
-        ConnectionType = _senor.ConnectionType;
     }
 
-    public string Id { get; set; }
-    public string Type { get; set; }
-    public string Keys { get; set; }
-    public string SensorType { get; set; }
-    public string ConnectionType { get; set; }
+    public string Id => _senor.Id.ToString();
+
+    public string Type
+    {
+        get => _senor.Type;
+        set
+        {
+            _senor.Type = value;
+            OnPropertyChanged(nameof(Type));
+        }
+    }
+
+    public string Keys
+    {
+        get => _senor.Keys;
+        set
+        {
+            _senor.Keys = value;
+            OnPropertyChanged(nameof(Keys));
+        }
+    }
+
+    public string SensorType
+    {
+        get => _senor.SensorType;
+        set
+        {
+            _senor.SensorType = value;
+            OnPropertyChanged(nameof(SensorType));
+        }
+    }
+
+    public string ConnectionType
+    {
+        get => _senor.ConnectionType;
+        set
+        {
+            _senor.ConnectionType = value;
+            OnPropertyChanged(nameof(ConnectionType));
+        }
+    }
 }
