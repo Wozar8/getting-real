1eb82d38b1aa62dab841edc7b0de17fac35e6cee
Wozar <2442498-wozar@users.noreply.gitlab.com>
2025-12-10 13:13:08 +0100

    Added sensor type validation

--- getting-real/Models/Sensor.cs
+++ getting-real/Models/Sensor.cs
@@ -1,33 +1,45 @@
-﻿namespace getting_real_4.Models;
+﻿using System.Diagnostics;
+
+namespace getting_real_4.Models;
 
 // TODO: Validation is missing
 
 public class Sensor
 {
-    public Sensor(string type, string keys, string sensorType, string connectionType, bool isHome)
-    {
-        // Id will be assigned by the repository when adding the sensor
-        Id = 0;
-        Type = type;
-        Keys = keys;
-        SensorType = sensorType;
-        ConnectionType = connectionType;
-        BatteryReplacementCount = 0;
-        IsHome = isHome;
-    }
+	public Sensor(string type, string keys, string sensorType, string connectionType, bool isHome)
+	{
+		validateSensorType(sensorType);
+		// Id will be assigned by the repository when adding the sensor
+		Id = 0;
+		Type = type;
+		Keys = keys;
+		SensorType = sensorType;
+		ConnectionType = connectionType;
+		BatteryReplacementCount = 0;
+		IsHome = isHome;
+	}
 
-    public int Id { get; set; }
-    public string Type { get; set; }
-    public string Keys { get; set; }
-    public string SensorType { get; set; }
-    public string ConnectionType { get; set; }
-    public bool IsHome { get; set; }
+	public int Id { get; set; }
+	public string Type { get; set; }
+	public string Keys { get; set; }
+	public string SensorType { get; set; }
+	public string ConnectionType { get; set; }
+	public bool IsHome { get; set; }
 
-    // Number of times the battery has been replaced for this sensor
-    public int BatteryReplacementCount { get; set; }
+	// Number of times the battery has been replaced for this sensor
+	public int BatteryReplacementCount { get; set; }
 
-    public void ReplaceBattery()
-    {
-        BatteryReplacementCount++;
-    }
+	public void ReplaceBattery()
+	{
+		BatteryReplacementCount++;
+	}
+
+	public void validateSensorType(string sensorType)
+	{
+		string loweredType = sensorType.ToLower();
+		if (loweredType != "ultralyd" && loweredType != "time of flight")
+		{
+			Debug.WriteLine($"Invalid sensor type: '{sensorType}'. Must be 'ultralyd' or 'time of flight'.");
+		}
+	}
 }
