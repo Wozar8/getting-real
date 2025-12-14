46cda6307a2206e41065b1efb6641692a4db8799
Wozar <2442498-wozar@users.noreply.gitlab.com>
2025-12-14 22:36:58 +0100

    Removed mountType

--- getting-real/Models/Battery.cs
+++ getting-real/Models/Battery.cs
@@ -2,16 +2,13 @@
 
 public class Battery
 {
-    public Battery(DateTime registerDate, string batterType, double maxCapacity, string mountType)
-    {
-        RegisterDate = registerDate;
-        BatteryType = batterType;
-        MaxCapacity = maxCapacity;
-        MountType = mountType;
-    }
-
-    public string BatteryType { get; set; }
-    public double MaxCapacity { get; set; }
-    public string MountType { get; set; }
-    public DateTime RegisterDate { get; set; }
+	public Battery(DateTime registerDate, string batterType, double maxCapacity)
+	{
+		RegisterDate = registerDate;
+		BatteryType = batterType;
+		MaxCapacity = maxCapacity;
+	}
+	public string BatteryType { get; set; }
+	public double MaxCapacity { get; set; }
+	public DateTime RegisterDate { get; set; }
 }
